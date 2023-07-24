b030ab1c6a1392c7da12bc43a5b42a786c7316f0
Igor <igor@cincatarina.sc.gov.br>
2023-07-24 20:39:30 -0300

    feat: cria UI do navbar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Container from '../Container'
+import Logo from './Logo'
+import Search from './Search'
+import UserMenu from './UserMenu'
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white shadow-sm">
+      <div className="border-b-[1px] py-4">
+        <Container>
+          <div className="flex flex-row items-center justify-between">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
   )
 }
